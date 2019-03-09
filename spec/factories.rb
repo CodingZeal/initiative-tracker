b02ce449016da6a24e17519bae59f63703edaae8
justinnealhatch <jhatch81@gmail.com>
2019-03-08 17:36:27 -0800

    Model and Integeration testing added

---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+    
+    factory :initiative do
+        title 'Test'
+        target_date '01/01/2020'
+    end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,6 +30,7 @@ ActiveRecord::Migration.maintain_test_schema!
 Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 RSpec.configure do |config|
   config.include ::Rails.application.routes.url_helpers
+  config.include FactoryBot::Syntax::Methods
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
