362579267719111f262718689a9878a076c4aea6
justgarcia5 <justgarcia5@gmail.comgit config --global user.email justgarcia5@gmail.com>
2019-03-13 15:12:39 -0700

    Rspec for new user page pass

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
-# require 'rails_helper'
-# require 'capybara/rspec'
+require 'rails_helper'
+require 'capybara/rspec'
 
-# feature 'Admin creates a new user' do
-#   scenario 'with fullname, email, is_admin, team_leader_id' do
-#     visit root_path
-#     fill_in 'Fullname', with: 'Test'
-#     fill_in 'Email', with: 'email@email.com'
+feature 'Admin creates a new user' do
+  scenario 'with fullname, email, is_admin, team_leader_id' do
+    visit new_user_path
+    fill_in 'Full Name', with: 'Test'
+    fill_in 'Email', with: 'email@email.com'
  
-#     click_button 'Add User'
-#     expect(page).to have_content('Test')
-#   end
-# end
+    click_button 'Submit'
+    expect(page).to have_content('Test')
+  end
+end
