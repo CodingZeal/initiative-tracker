2e9b15c61b0d2b2f60064115fb8458a339390c71
justgarcia5 <justgarcia5@gmail.comgit config --global user.email justgarcia5@gmail.com>
2019-03-15 15:35:14 -0700

    Finished integrated testing, deleted show.html.erb and resolved jason's comments

--- spec/factories.rb
+++ spec/factories.rb
@@ -9,6 +9,5 @@ FactoryBot.define do
   factory :user do
     fullname 'Test'
     email 'email@email.com'
-    # password 'password'
   end
 end
--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,17 +1,53 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-require 'capybara/rspec'
+require 'capybara/rspec' 
+
+feature 'Admin can see a list of users' do
+  scenario 'clicks user list' do
+    visit root_path
+    click_link 'User List'
+    expect(page).to have_content('Users')
+  end
+end
 
 feature 'Admin creates a new user' do
-  scenario 'with all attributes' do
+  scenario 'clicks add user button' do
+    visit users_path
+    click_link 'Add User'
+    expect(page).to have_content('New User')   
+  end
+  scenario 'with attributes for Admin' do
     visit new_user_path
     fill_in 'Full Name', with: 'Test'
     fill_in 'Email', with: 'email@email.com'
-    fill_in 'Is Admin', with: 'false'
-    fill_in 'Team Leader', with: '2'
- 
+    check 'Is Admin'
     click_button 'Submit'
     expect(page).to have_content('Test')
+    expect(page).to have_content('email@email.com')
+    expect(page).to have_content('Admin')
+  end
+  scenario 'with attributes for Team Leader' do
+    visit new_user_path
+    fill_in 'Full Name', with: 'Test1'
+    fill_in 'Email', with: 'email1@email.com'
+    fill_in 'Team Leader', with: '1'
+    click_button 'Submit'
+    expect(page).to have_content('Test1')
+    expect(page).to have_content('email1@email.com')
+    expect(page).to have_content('Team Leader')
+  end
+  scenario 'with attributes for Team Member' do
+    visit new_user_path
+    fill_in 'Full Name', with: 'Test2'
+    fill_in 'Email', with: 'email2@email.com'
+    click_button 'Submit'
+    expect(page).to have_content('Test2')
+    expect(page).to have_content('email2@email.com')
+    expect(page).to have_content('Team Member')
   end
 end
+
+    
+
+
