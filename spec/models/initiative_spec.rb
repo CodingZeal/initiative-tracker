bcffa66f5e0f47dca9a75911fa490e758d23352d
kevinyu05 <kevinyu05@gmail.com>
2019-03-18 10:38:15 -0700

    testing WIP

--- spec/models/initiative_spec.rb
+++ spec/models/initiative_spec.rb
@@ -18,27 +18,3 @@ RSpec.describe Initiative, type: :model do
     end
   end
 end
-
-describe InitiativesController, type: :controller do
-  let!(:initiative) { create(:initiative) }
-
-  it "deletes the initiative" do
-    expect{ delete :destroy, params: {id: initiative.id}}.to change(Initiative,:count).by(-1)
-    expect(response).to redirect_to root_path
-  end
-end
-
-describe "#index" do
-  it "orders by initiative status" do
-    @initiative = Initiative.new(title: 'initiative1', target_date: "01/01/2020", completion: true)
-    @initiative.save
-    
-    @initiative1 = Initiative.new(title: 'initiative2', target_date: "01/01/2020")
-    @initiative1.save
-
-    initt = Initiative.order('completion DESC').all.to_a
-    init = [@initiative1, @initiative]
-
-    expect(initt).to eq(init)
-  end
-end
