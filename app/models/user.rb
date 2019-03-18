4a965ac52f55160759f5406f2f8f77d33c1ff8c9
kevinyu05 <kevinyu05@gmail.com>
2019-03-18 16:41:45 -0700

    added css for checkbox and dropdown, added dropdown functionality, added required password field, added logic to user model, testing WIP

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+  devise :database_authenticatable
+  
   validates :fullname, presence: true
   validates :email, presence: true
+  validates :password, presence: true
+
+  has_many :team_members, class_name: "User", foreign_key: "team_leader_id"
+  belongs_to :team_leader, class_name: "User", required: false
 end
