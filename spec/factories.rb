61c22d32ee9884ab6b1f0552657ade83c9429d4d
justgarcia5 <justgarcia5@gmail.comgit config --global user.email justgarcia5@gmail.com>
2019-03-19 09:15:54 -0700

    completed initiative controller testing

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,5 +4,6 @@ FactoryBot.define do
   factory :initiative do
     title 'Test'
     target_date '01/01/2020'
+    completion false
   end
 end
--- spec/features/initiative_spec.rb
+++ spec/features/initiative_spec.rb
@@ -3,12 +3,11 @@
 require 'rails_helper'
 require 'capybara/rspec'
 
-
 feature 'Visitor creates a new initiative' do
   scenario 'clicks new initiative button' do
     visit root_path
     click_link 'New Initiative'
-    expect(page).to have_content('New Initiative')   
+    expect(page).to have_content('New Initiative')
   end
   scenario 'with title target date and description' do
     visit new_initiative_path
@@ -20,9 +19,9 @@ feature 'Visitor creates a new initiative' do
   end
 end
 
-feature 'Visitor edits a initiative' do 
+feature 'Visitor edits a initiative' do
   background do
-    @initiative = Initiative.create!(:title => 'TestA', :target_date => '01/02/2020', :description => 'ice cream')
+    @initiative = Initiative.create!(:title => 'TestA', :target_date => '01/02/2020', :description => 'ice cream', :completion => false)
   end
   scenario 'clicks initiative on list view' do
     visit edit_initiative_path(@initiative)
@@ -42,13 +41,13 @@ feature 'Visitor edits a initiative' do
   end
   scenario "delete initiative from list view" do
     visit root_path
-    expect { click_link '', :style => "text-decoration: none;" }.to change(Initiative, :count).by(-1)
+    click_on(class: 'no-link')
     expect(page).to_not have_content('ice cream')
   end
   scenario "display flash message confirming initiative deletion" do
     visit root_path
-    expect { click_link '', :style => "text-decoration: none;" }.to change(Initiative, :count).by(-1)
-    expect(page).to have_content('deleted')    
+    expect { click_link('delete-initiative' )}.to change(Initiative, :count).by(-1)
+    expect(page).to have_content('deleted')
   end
   scenario 'click a completion checkbox' do
     visit edit_initiative_path(@initiative)
