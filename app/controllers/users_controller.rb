e3bf555545182fac57cb69b2f39b568338b459d9
justinnealhatch <jhatch81@gmail.com>
2019-03-19 15:00:28 -0700

    Corrected user controller alignment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,11 +11,10 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
-      if @user.save
-        redirect_to :users
-      else
-        render :new
+    if @user.save
+      redirect_to :users
+    else
+      render :new
     end
   end
 
