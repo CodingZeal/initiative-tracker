328da6d7d087763d650399309fff665ba8f609d3
kevinyu05 <kevinyu05@gmail.com>
2019-03-21 13:49:44 -0700

    user menu only appears for Admin, user management functionality available for admin, flash message for non-admin implemented, testing WIP

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+	def require_admin
+		unless current_user && current_user.is_admin
+			flash[:notice] = "You are not an admin"
+			redirect_to root_path
+		end
+	end
 end
