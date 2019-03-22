81b28654260124d07656924915ea861add182d81
H. Mike Ko <hyosunko@gmail.com>
2019-03-22 16:14:03 -0700

    fix indentations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 class ApplicationController < ActionController::Base
-	def require_admin
-		unless current_user&.is_admin
-			flash[:notice] = "You are not an admin"
-			redirect_to root_path
-		end
-	end
+  def require_admin
+    unless current_user&.is_admin
+      flash[:notice] = "You are not an admin"
+      redirect_to root_path
+    end
+  end
 end
