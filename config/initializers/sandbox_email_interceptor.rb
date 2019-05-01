40dd3cdad46d60583c3e1dadd3a218b2a0c4757f
Mike <hyosunko@gmail.com>
2019-04-30 22:42:42 -0700

    deleted interceptor file
    We have change test mail method to mail catcher, and we don't need it anymore.

--- config/initializers/sandbox_email_interceptor.rb
+++
@@ -1,8 +0,0 @@
-if Rails.env.development?
-  class SandboxEmailInterceptor
-    def self.delivering_email(message)
-      message.to = ['hyosunko@gmail.com']
-    end
-  end
-  ActionMailer::Base.register_interceptor(SandboxEmailInterceptor)
-end
