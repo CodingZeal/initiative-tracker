803d7945b4794aa9444e2bb793a22aeb32540f04
Hyosun Ko <hyosunko@gmail.com>
2019-05-07 18:29:18 +0000

    mailing setup change to mailgun

--- config/environment.rb
+++ config/environment.rb
@@ -6,11 +6,11 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'codingzeal.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'codingzeal.com',
   :authentication => :plain,
-  :enable_starttls_auto => true
 }
+ActionMailer::Base.delivery_method = :smtp
