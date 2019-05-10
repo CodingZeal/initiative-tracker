f7fdbe7379f6c50646ead10a98f89a0c2f5108a8
Hyosun Ko <hyosunko@gmail.com>
2019-05-10 00:59:10 +0000

    fix url setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,5 +94,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'initiative-tracker-stagi-pr-23' }
+  #config.action_mailer.default_url_options = { :host => 'initiative-tracker-stagi-pr-23' }
 end
